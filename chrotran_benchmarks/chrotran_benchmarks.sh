6818f2d06dd36e1119c665793a7768ce4de55430
Sachin Pandey <sach@lanl.gov>
2017-09-19 11:35:57 -0600

    Added microbe clogging benchmark and removed reduntant benchmarks.

--- chrotran_benchmarks/chrotran_benchmarks.sh
+++ chrotran_benchmarks/chrotran_benchmarks.sh
@@ -1,47 +1,52 @@
 #!/bin/bash
 
+echo running abiotic
 cd abiotic
-# ../../src/pflotran/chrotran -pflotranin abiotic.in
-# python abiotic.py
-# 
-# cd ../abiotic_mimt
-# ../../src/pflotran/chrotran -pflotranin abiotic_mimt.in
-# python abiotic_mimt.py
-# 
-# cd ../microbe_biocide
-# ../../src/pflotran/chrotran -pflotranin microbe_biocide.in
-# python microbe_biocide.py
-# 
-# cd ../microbe_decay
-# ../../src/pflotran/chrotran -pflotranin microbe_decay.in
-# python microbe_decay.py
-# 
-# cd ../microbe_enzymatic
-# ../../src/pflotran/chrotran -pflotranin microbe_enzymatic.in
-# python microbe_enzymatic.py
-# 
-# cd ../microbe_growth
-# ../../src/pflotran/chrotran -pflotranin microbe_growth.in
-# python microbe_growth.py
-# 
-# cd ../microbe_growth_decay
-# ../../src/pflotran/chrotran -pflotranin microbe_growth_decay.in
-# python microbe_growth_decay.py
+../../src/pflotran/chrotran -pflotranin abiotic.in >/dev/null 2>/dev/null
+python abiotic.py
 
+echo running abiotic_mimt
+cd ../abiotic_mimt
+../../src/pflotran/chrotran -pflotranin abiotic_mimt.in >/dev/null 2>/dev/null
+python abiotic_mimt.py
+
+echo running microbe_biocide
+cd ../microbe_biocide
+../../src/pflotran/chrotran -pflotranin microbe_biocide.in >/dev/null 2>/dev/null
+python microbe_biocide.py
+
+echo running microbe_enzymatic
+cd ../microbe_enzymatic
+../../src/pflotran/chrotran -pflotranin microbe_enzymatic.in >/dev/null 2>/dev/null
+python microbe_enzymatic.py
+
+echo running microbe_growth_decay
+cd ../microbe_growth_decay
+../../src/pflotran/chrotran -pflotranin microbe_growth_decay.in >/dev/null 2>/dev/null
+python microbe_growth_decay.py
+
+echo running microbe_growth_inhibitor
 cd ../microbe_growth_inhibitor
-../../src/pflotran/chrotran -pflotranin microbe_growth_noinhibitor.in
-../../src/pflotran/chrotran -pflotranin microbe_growth_inhibitor.in
+../../src/pflotran/chrotran -pflotranin microbe_growth_noinhibitor.in >/dev/null 2>/dev/null
+../../src/pflotran/chrotran -pflotranin microbe_growth_inhibitor.in >/dev/null 2>/dev/null
 python microbe_growth_inhibitor.py
 
-# cd ../microbe_respiration
-# ../../src/pflotran/chrotran -pflotranin microbe_respiration.in
-# python microbe_respiration.py
-# 
-# cd ..
-# 
-# if [ -d "results_benchmark" ]; then
-#   rm -r results_benchmark
-# fi
-# 
-# mkdir results_benchmark
-# mv ./*/*.png results_benchmark
+echo running microbe_respiration
+cd ../microbe_respiration
+../../src/pflotran/chrotran -pflotranin microbe_respiration.in >/dev/null 2>/dev/null
+python microbe_respiration.py
+
+echo running microbe_clogging
+cd ../microbe_clogging
+../../src/pflotran/chrotran -pflotranin microbe_clogging.in >/dev/null 2>/dev/null
+python microbe_clogging.py
+
+cd ..
+
+if [ -d "results_benchmark" ]; then
+  rm -r results_benchmark
+fi
+
+mkdir results_benchmark
+mv ./*/*.png results_benchmark
+echo results are in results_benchmark directory
---
+++ chrotran_benchmarks/microbe_clogging/microbe_clogging.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+from numpy import ma
+import h5py
+
+filename = 'microbe_clogging.h5'
+f = h5py.File(filename, 'r')
+
+timesteps = range(1,200)
+dt = 1.0
+myvars = ['biomass [mol_m^3]','Permeability_X [m^2]','Porosity']
+majorFormatter = plt.matplotlib.ticker.FormatStrFormatter("%0.1e")
+
+keys = f.keys()
+timekeys = [key for key in keys if 'Time:' in key]
+times = [float(timekey.split('Time:  ')[1].split(' d')[0]) for timekey in timekeys]
+st = sorted(times)
+keydict = dict(zip(times,timekeys))
+
+# get coordinates
+xgrid = f['Coordinates']['X [m]'].value
+ygrid = f['Coordinates']['Y [m]'].value
+# x, y = np.meshgrid(xgrid, ygrid)
+x, y = np.mgrid[min(xgrid):max(xgrid):xgrid[1]-xgrid[0], min(ygrid):max(ygrid):ygrid[1]-ygrid[0]]
+
+results = dict()
+for myvar in myvars:
+    results[myvar] = []
+    for timestep in timesteps:
+        # get values at the well (center node)
+        results[myvar].append(f[keydict[timestep*dt]][myvar].value[1][1][0])
+
+f,ax = plt.subplots(1,3,figsize=(10,4))
+for i in range(0,3):
+    ax[i].plot([timestep*dt for timestep in timesteps],results[myvars[i]])
+    ax[i].set_ylabel(myvars[i])
+    ax[i].set_xlabel('Time [day]')
+
+ax[0].yaxis.set_major_formatter(majorFormatter)
+ax[1].yaxis.set_major_formatter(majorFormatter)
+plt.tight_layout()
+f.savefig('microbe_clogging.png')
